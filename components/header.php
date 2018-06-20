56c985c93b11bf4b62b9c8da00447f2547ae30bd
ananta <anantarajbastola@gmail.com>
2018-06-21 00:56:57 +0545

    minor changes in header

--- components/header.php
+++ components/header.php
@@ -5,26 +5,33 @@
 ?>
 <!DOCTYPE html>
 <html lang="en" >
-
 <head>
-  <meta charset="UTF-8">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin Login</title>
       <link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 <body>
-
 <header>
+	<div class="toggle">
+		<img src="res/hamburger.png" alt="icon">
+	</div>
 	<div class= "menu">
 		<a class="brand" href="home.php">APP SERVER</a>
 	</div>
 	<?php 
 	if (!isset($_SESSION["login_user"])){
-		echo "<nav><ul><li><a href='login.php'>Please Login</a></li></ul></nav>";
+		echo '
+		<nav>
+			<ul>
+			<li><a class="'.($currentPage == "teachers.php" ? "active" : "inactive").'" href="teachers.php">Teachers</a></li>
+			</ul>
+		</nav>';
 	  }else{
 		echo '
 		<nav>
 			<ul>
-				<li><a class="'.($currentPage == "add_teachers.php" ? "active" : "inactive").'" href="add_teachers.php">Add Teachers</a></li>
+				<li><a class="'.($currentPage == "add_teachers.php" ? "active" : "inactive").'" href="add_teachers.php">Teachers</a></li>
 				<li><a class="'.($currentPage == "resources.php" ? "active" : "inactive").'" href="resources.php">Resources</a></li>
 				<li><a class="'.($currentPage == "results.php" ? "active" : "inactive").'" href="results.php">Result</a></li>
 				<li><a class="'.($currentPage == "events.php" ? "active" : "inactive").'"  href="events.php">Events</a></li>
