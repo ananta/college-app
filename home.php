5c7731e4a7cffee62254b7bbcd80de397b10bf53
ananta <anantarajbastola@gmail.com>
2018-06-21 00:55:25 +0545

    Added Profile Card

--- home.php
+++ home.php
@@ -1,10 +1,23 @@
 <?php
     include('utils/session.php');
     include('components/header.php');
-    echo $currentPage;
 ?>
 
-    <h1>Hello World</h1>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="res/avatar.png" alt="Avatar Image" style="width:100%">  
+                <h1 class="cardTitle"><?php echo $_SESSION['name'];?></h1>
+                <h4 class="cardUsername">@<?php echo $_SESSION['login_user']?></h4>
+                <h4 class="cardEmail"><?php echo $_SESSION['email']?></h4>
+                <button class="button buttonGreen">Edit Profile</button>
+            </div>
+        </div>
+        <div class="column">
+            
+        </div>
+    
+    </div>
 <?php 
     include('components/footer.php');
 ?>
