65f3108efb5320077d7211535b9c12a281e871bf
ananta <anantarajbastola@gmail.com>
2018-08-09 16:00:27 +0545

    update home page

--- home.php
+++ home.php
@@ -1,19 +1,22 @@
 <?php
-    include('utils/session.php');
+
     include('components/header.php');
+    include('utils/session.php');
 ?>
 
     <div class="row">
-        <div class="column" style="width:35%; min-width:400px;">
+        <div class="column">
             <div class="card">
-                <img src="res/avatar.png" alt="Avatar Image" style="width:100%">  
+                <img src=' <?php $imgPath = ($_SESSION['profileImg']) ? ($mainPage."uploads/users/profile-picture/".$_SESSION['profileImg']) : $mainPage."res/avatar.png"; echo $imgPath; ?>' alt="Avatar Image" >  
                 <h1 class="cardTitle"><?php echo $_SESSION['name'];?></h1>
                 <h4 class="cardUsername">@<?php echo $_SESSION['login_user']?></h4>
                 <h4 class="cardEmail"><?php echo $_SESSION['email']?></h4>
-                <button class="button buttonGreen">Edit Profile</button>
+                <a href="http://localhost/gcesServer/pages/admin/admin_edit_profile.php">
+                    <button class="button buttonGreen">Edit Profile</button>
+                </a>
             </div>
         </div>
-        <div class="column" style="width:65%;">
+        <div class="column">
             <div class="homeGridMenu">
                 <?php 
                 if($_SESSION["login_user"] == "admin"){
@@ -23,7 +26,7 @@
                 ?>                
                 <div class="moduleItem"><a href="pages/admin/admin_events.php">Events</a></div>
                 <div class="moduleItem"><a href="pages/admin/admin_notices.php">Notices</a></div>
-                <div class="moduleItem"><a href="pages/admin/admin_result.php">Result</a></div>
+                <div class="moduleItem"><a href="pages/admin/admin_results.php">Result</a></div>
                 <div class="moduleItem"><a href="pages/admin/admin_resources.php">Resources</a></div>
             </div>
         </div>
