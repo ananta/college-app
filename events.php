7437099e1c69271fbde75a69c6fa76595848f2ca
ananta <anantarajbastola@gmail.com>
2018-08-09 16:00:20 +0545

    update events page

--- events.php
+++ events.php
@@ -22,24 +22,35 @@
         ';
     }else {
         while($row = mysqli_fetch_array($result)){
+            $date_time = explode(" ",$row["event_date"]);
+            $time = $date_time[1];
+            $date = $date_time[0];
+            $time = explode(":",$time);
             echo '
+            <div style="margin:20px;">
             <div class="eventCard">
                 <h2 class="eventTitle">'.$row["event_title"].'</h2>
-                <h5 class="eventSubTitle"><img src="res/location_icon.png" style="width:15px;"> '.$row["event_venue"].'</h5>
-                <h5 class="eventTime"><img src="res/clock_icon.png" style="width:15px;"> '. $row["event_date"].'</h5>
+                <div class="eventDescription">
+                    <p class="eventDetails">'.$row["event_description"].'</p>
+                </div>
+                <div style="display:inline-block; padding:8px;">
+                    <h5 class="eventSubTitle" ><img src="res/location_icon.png" style="width:12px; "> '.$row["event_venue"].' | <img src="res/clock_icon.png" style="width:12px; margin-right:5px;">'.$date.' | <img src="res/clock_icon.png" style="width:12px; margin-right: 5px">'.$time[0].':'.$time[1].'</h5>
+                </div>
                 <div class="eventImg">
                     <a href="uploads/events/'. ($row["event_image"] ? $row["event_image"] : "default.png") .'">
-                    <img style="width:100%; height:200px;"src="uploads/events/'. ($row["event_image"] ? $row["event_image"] : "default.png") .'" alt="image">
+                    <img style="width:100%; height:50%"src="uploads/events/'. ($row["event_image"] ? $row["event_image"] : "default.png") .'" alt="image">
                     </a>
                 </div>
-                <p class="eventDetails">'.$row["event_description"].'</p>
                 <div style="height:2px; background-color:gray;"></div>
                 <div class="eventFooter">
-                    <p style="color:gray; float:left;">Added By: @'.$row["added_by"].'</p>
+                    <p style="color:gray; float:left;">Added By: <a href="'.$mainPage.'view_teacher.php?teacherID='.$row["added_by"].'">@'.$row["added_by"].'</a></p>
                     <p style="color:gray; float:right;" >Created On : '.$row["event_created"].'</p>
+                    <p style="clear:both;"></p>
                 </div>
                 <div></div>
+            </div>
             </div>'
+
                 ;
         }
     }
